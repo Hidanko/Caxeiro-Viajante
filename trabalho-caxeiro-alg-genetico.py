64e0592559c08af7e7f43a28bc2e3c978d566d12
Gustavo Nemeth <hidan_nemeth@hotmail.com>
2019-06-19 16:01:54 -0300

    Add files via upload

--- trabalho-caxeiro-alg-genetico.py
+++ trabalho-caxeiro-alg-genetico.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 class Aresta:
     origem = -1
     destino = -1
@@ -11,6 +13,10 @@ class Aresta:
 
 # Caminho para o txt
 txt = 'C:/Users/Sattra/Documents/str-integracoes/endpoints - Poly/dist/movimentacoes/teste.txt'
+geracoes = 50
+num_populacoes = 5
+# taxa de mutacao: 2 = 2%
+mutacao = 2
 # Alfabeto com 20 caracteres para representar até 20 cidades
 alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
 arestas = []
@@ -33,9 +39,32 @@ for obj in objetos[1:num_cidades+1]:
     arestas.append(Aresta(cont, cont+1, obj))
     cont = cont+1
 
-for obj in objetos[num_cidades+1:]:
+#for obj in objetos[num_cidades+1:]:
     # gerar arestas em direções aleatórias com valores que sobraram
 
 # for a in arestas:
 #    print(a.destino)
+populacao = list
+populacao_peso = list
+modelo = [[i] for i in range(num_cidades-1)]
+
+for p in num_populacoes:
+    shuffle(modelo)
+    populacao.append(modelo)
+    populacao_peso.append(-1)
+
+nova_geracao = list
+for geracao in range(0, geracoes):
+    for p in populacao:
+        flag_geral = True
+        for pp in range(populacao.__sizeof__()-1):
+            flag = False
+            for a in arestas:
+                if a.origem == populacao[pp] and a.destino == populacao[pp]:
+                    flag = True
+                    break
+            if flag == False:
+                flag_geral = False
+            else:
+                nova_geracao.append(populacao)
 
